6b2a4cf303dc07654601f4df46e1ae45a0198cb7
Thiago Rossi Lazarin <thiagolazarin08@gmail.com>
2025-01-08 22:56:43 -0300

    testando

--- run_data.py
+++ run_data.py
@@ -1,7 +1,6 @@
 from app.utils.init_data import main as run_scraping
 
 if __name__ == "__main__":
-    # Passo 1: Executar o init_data.py (garantir que isso só rode na primeira inicialização)
     print("Executando scraping e inicializando dados...")
     try:
         run_scraping()
